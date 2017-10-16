af98326df62ad063c368d098f24984e0bf4b6180
frpele <frpele@gmail.com>
2017-10-16 11:40:12 +0200

    commit

--- js/contact.js
+++ js/contact.js
@@ -21,10 +21,8 @@ if   (document.formul.firstname.value == "") {
     alert("Attention, le champ prénom doit être rempli ");
     document.formul.firstname.focus();
     document.formul.firstname.style.backgroundColor = "red";
-   return false;
-
-    
-} 
+   return false; 
+    } 
     
 var lastname  = document.getElementById('lastname').value;
 console.log(lastname);    
@@ -33,46 +31,59 @@ if   (document.formul.lastname.value == "") {
     alert("Attention, le champ nom doit être rempli ");
     document.formul.lastname.focus();
     document.formul.lastname.style.backgroundColor = "red";
-   return false;
+   return false;   
+    } 
 
-    
-} 
+var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,3})+$/;
+if (regex.test(document.getElementById('email').value))  
+    {  
+    return true;
+     } else{
+     alert("Entrez email valide!");
+    document.formul.email.focus();
+    document.formul.email.style.backgroundColor = "#f2dede";
+    return false;
+    }
 
-var email  = document.getElementById('email').value;
+
+/*var email  = document.getElementById('email').value;
 console.log(email);    
     
 if   (document.formul.email.value == "") {
     alert("Attention, le champ email doit être rempli ");
     document.formul.email.focus();
     document.formul.email.style.backgroundColor = "red";
-   return false;
-
-    
-} 
-
+   return false;    
+    } 
+*/
 var tel  = document.getElementById('tel').value;
-console.log(lastname);    
+console.log(tel);    
     
 if   (document.formul.tel.value == "") {
     alert("Attention, le champ tel doit être rempli ");
     document.formul.tel.focus();
     document.formul.tel.style.backgroundColor = "red";
-   return false;
-
-    
-} 
+   return false;    
+    } 
     
 var message  = document.getElementById('message').value;
-console.log(lastname);    
+console.log(message);    
 
 if   (document.formul.message.value == "") {
     alert("Attention, le champ message doit être rempli ");
     document.formul.message.focus();
     document.formul.message.style.backgroundColor = "red";
-   return false;
+   return false;    
+    } 
 
-    
-} 
+var checked = document.getElementById('tnc').checked;
+    if (!checked )  {
+   
+      alert("vous devez accepté les mentions légales !");
+      document.formul.tnc.focus();
+      document.formul.tnc.style.backgroundColor = "#f2dede";
+      return false;
+    }
 
 }
 
