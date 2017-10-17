6209621f707a6af4aa4d1184ae91e236c11462c4
amare <amarenighatu16@gmail.com>
2017-10-17 09:49:03 +0200

    Js-validation-form is completed

--- js_validation_form/js/script.js
+++ js_validation_form/js/script.js
@@ -1,18 +1,94 @@
 
 function validate() {
 
+    // Declare variables
+
     var male = document.getElementById('male').checked;
     var female = document.getElementById('female').checked;
 
-    var Firstname = document.getElementById('first-name').value;
-    var Lastname = document.getElementById('last-name').value;
+    var Firstname = document.getElementById('first_name').value;
+    var Lastname = document.getElementById('last_name').value;
     var Email = document.getElementById('email').value;
     var Tel = document.getElementById('tel').value;
-    var Terms = document.getElementById("terms").checked;
+    var Agree = document.getElementById("agree").checked;
+    var Read = document.getElementById("read").checked;
     var Message = document.getElementById('message').value;
 
-    var emailregex = /^[a-zA-Z0-9._-]+\.[a-zA-Z]{2,6}$/;
-    var telregex = /^(01|02|03|04|05|06|08|0033|\+33)[0-9]{8}/;
+    var email_regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,3})+$/; // /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var tel_regex = /^(01|02|03|04|05|06|08|0033|\+33)[0-9]{8}/;
+
+    // Gender button
+    if (male === false && female === false) {
+        alert("Please select your gender!");
+        document.form.male.focus();
+        document.form.male.style.backgroundColor = "#f2dede";
+        return false;
+    }
+
+    // First name
+    if (Firstname === "") {
+        alert("Please enter your first name!");
+        document.form.first_name.focus();
+        document.form.first_name.style.backgroundColor = "#f2dede";
+        return false;
+    }
+
+    // Last name
+    if (Lastname === "") {
+        alert("Please enter your last name!");
+        document.form.last_name.focus();
+        document.form.last_name.style.backgroundColor = "#f2dede";
+        return false;
+    }
+
+    // Email
+    if (!email_regex.test(Email)) {
+        alert("Please enter your email!");
+        document.form.email.focus();
+        document.form.email.style.backgroundColor = "#f2dede";
+        return false;
+    }
+
+
+    // Telephone
+    if(!tel_regex.test(Tel)) {
+        alert("Please enter your telephone number!");
+        document.form.tel.focus();
+        document.form.tel.style.backgroundColor = "#f2dede";
+        return false;
+    }
+
+    // Message
+    if (Message === "" || Message.length > 10) {
+        alert("Please enter your message maximum 10 characters!");
+        document.form.message.focus();
+        document.form.message.style.backgroundColor = "#f2dede";
+        return false;
+    }
+
+    // Terms and Conditions
+    if (Agree === false && Read === false) {
+        alert("Please accept terms and conditions or read it!");
+        document.form.agree.focus();
+        document.form.agree.style.backgroundColor = "#f2dede";
+        return false;
+    }
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+   /*
 
     if (Firstname != "" && Lastname != "" && Email != "" && Tel != "" && Message != "") {
         if (document.getElementById("first-name").value && document.getElementById("last-name")) {
@@ -59,6 +135,7 @@ function validate() {
         return false;
     }
 }
+*/
 
 
 
