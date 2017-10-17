311f1c99c59e26de609b10f4b836cea29cd58cd6
Lea Rerolle <lea.rerolle@gmail.com>
2017-10-18 00:37:22 +0200

    js du panier ok

--- panier/basket.js
+++ panier/basket.js
@@ -1,18 +1,46 @@
-function totalpriceperitem()
-{
-	var facturations = document.querySelectorAll(".facturations li")
+var totalprices = document.querySelectorAll(".totalprice"); 
+var totalbasket = document.querySelector(".totalbasket");
 
-	facturations.forEach(function(facture){
-		var price = facture.querySelector(".price");
-		var quantity = facture.querySelector(".quantity");
+document.querySelectorAll('.facturations li').forEach(function (facture) {
+	var price = facture.querySelector('.price');
+	var quantity = facture.querySelector('.quantity');
+	var totalprice = facture.querySelector('.totalprice');
 
+	var listener = function () {
 		if((quantity.value != "" && !isNaN(quantity.value)) && (price.value != "" && !isNaN(price.value)) ) {
 			var calcul = price.value * quantity.value;
 
-			facture.querySelector(".totalprice").value = calcul;
+			totalprice.value = calcul;
+			var sum = 0;
+
+			totalprices.forEach(function (total) {
+				sum = sum + parseInt(total.value, 10);
+			})
+			totalbasket.value = sum;
 		}
-	});
-}
+	}
+
+	price.onkeyup = listener;
+	quantity.onkeyup = listener;
+})
+
+
+// function totalpriceperitem(event) {
+
+// 	console.log(event.target);
+// 	var facturations = document.querySelectorAll(".facturations li")
+
+// 	facturations.forEach(function(facture){
+// 		var price = facture.querySelector(".price");
+// 		var quantity = facture.querySelector(".quantity");
+
+// 		if((quantity.value != "" && !isNaN(quantity.value)) && (price.value != "" && !isNaN(price.value)) ) {
+// 			var calcul = price.value * quantity.value;
+
+// 			facture.querySelector(".totalprice").value = calcul;
+// 		}
+// 	});
+// }
 
 
 /*{
