956707e863fed818bbd13e54a716c4a1af00ce66
Jeannine <chrissrakoto@gmail.com>
2017-10-19 15:46:34 +0200

    2nd try

--- TP_JS/js/javascript.js
+++ TP_JS/js/javascript.js
@@ -5,52 +5,50 @@ function verif(nom,prenom,mail){
   nom = $("#nom").val();
   prenom = $("#prenom").val();
   mail = $("#mail").val();
-// si vide :
-  if (nom === ""){
+  var regex =	/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (nom === "" || !isNaN(nom)){
       msgError.html("\*Vous devez renseigner correctement les champs !");
       msgError.css("background-color","#ff5f57");
       $("#nom").focus();
       $("#nom").css("background-color","#f2dede");
       return false;
     }
-  else if (prenom === ""){
-      msgError.html("\*Vous devez renseigner correctement les champs  !");
-      msgError.css("background-color","#ff5f57");
-      $("#prenom").focus();
-      $("#prenom").css("background-color","#f2dede");
-      return false;
-    }
-  else if (mail === ""){
-      msgError.html("\*Vous devez renseigner correctement les champs ! !");
-      msgError.css("background-color","#ff5f57");
-      $("#mail").focus();
-      $("#mail").css("background-color","#f2dede");
-      return false;
-    }
-  else{// si tout bon rajouter ligne :
-    console.log(nom,prenom,mail);
-    $('tabl_eleve').find('tbody').append([
-            '<tr>',
-                '<td>'+2+'</td>',
-                '<td>'+nom+'</td>',
-                '<td>'+prenom+'</td>',
-                '<td>'+mail+'</td>',
-            '</tr>'
-            ].join(''));
+    if (prenom === "" || !isNaN(prenom)){
+        msgError.html("\*Vous devez renseigner correctement les champs  !");
+        msgError.css("background-color","#ff5f57");
+        $("#prenom").focus();
+        $("#prenom").css("background-color","#f2dede");
+        return false;
+      }
+
+    if (mail === ""){
+          msgError.html("\*Vous devez renseigner correctement les champs ! !");
+          msgError.css("border-color","red");
+          $("#mail").focus();
+          $("#mail").css("background-color","#f2dede");
+          return false;
+    }else if (!regex.test(mail)){
+        msgError.html("\*Mail incorrect ! (ex: utilisateur@mail.com)");
+        msgError.css("background-color","#be57ff");
+        $("#mail").css("background-color","#eed2ff");
+        return false;
+    }else {// si tout bon rajouter ligne :
+    ajoutLigne(nom,prenom,mail);
   }
 }
-
-/*function insertEleve(nom,prenom,mail){
+function ajoutLigne(nom,prenom,mail){
   nom = $("#nom").val();
   prenom = $("#prenom").val();
   mail = $("#mail").val();
-$('tabl_eleve').find('tbody').append([
-        '<tr>',
-            '<td>'+2+'</td>',
-            '<td>'+nom+'</td>',
-            '<td>'+prenom+'</td>',
-            '<td>'+mail+'</td>',
-        '</tr>'
-        ].join(''));
+var tableau = $("#tab_eleve");
+var ligne = tableau.insertRow(-1);
+var colonne1 = ligne.insertCell(0);
+colonne1.innerHTML += "2";
+var colonne2 = ligne.insertCell(1);
+colonne2.innerHTML += nom;
+var colonne3 = ligne.insertCell(2);
+colonne3.innerHTML += prenom;
+var colonne4 = ligne.insertCell(3);
+colonne4.innerHTML += mail;
 }
-*/
--- TP_JS/js/op_tp.js
+++
@@ -1,8 +0,0 @@
-$('tabl_eleve').find('tbody').append([
-'<tr>',
-    '<td>'+2+'</td>',
-    '<td>'+nom+'</td>',
-    '<td>'+prenom+'</td>',
-    '<td>'+mail+'</td>',
-'</tr>'
-].join(''));
