de9215b4dab92103f9e2d79d4c7ae52ff22aab83
Rerolle <lea.rerolle@gmail.com>
2017-10-19 13:05:53 +0200

    CSS à revoir

--- liste-etudiants/js/etudiants.js
+++ liste-etudiants/js/etudiants.js
@@ -1,24 +1,63 @@
+var borderFirstName = $("#firstname");
+var borderLastName = $("#lastname"); 
+var borderEmail = $("#email"); 
+var regex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+
+borderFirstName.on( "blur", function() {
+	if (this.value === "" || !isNaN (this.value))
+		$( this ).css( "border", "1px solid red");
+	else 
+		$( this ).css( "border", "1px solid LightGreen");
+});
+
+borderLastName.on( "blur", function() {
+	if (this.value === "" || !isNaN (this.value))
+		$( this ).css( "border", "1px solid red");
+	else 
+		$( this ).css( "border", "1px solid LightGreen");
+});
+
+borderEmail.on( "blur", function() {
+	if (this.value === "" || !regex.test(this.value))
+		$( this ).css( "border", "1px solid red");
+	else 
+		$( this ).css( "border", "1px solid LightGreen");
+});
+
 function verifyForm () {
 	var firstname = document.getElementById("firstname").value;
 	var lastname = document.getElementById("lastname").value;
 	var email = document.getElementById("email").value;
+	
 
-	if (firstname === "") {
+	if ((firstname === "" || !isNaN(firstname)) || (lastname === "" || !isNaN(lastname)) || (email ==="" || !regex.test(email))) 
 		return false;
-	}
-	if (lastname === "") {
-		return false;
-	}
 
-	if (email === "") {
-		return false
+	return true;
+}
+function verifEmail () {
+
+	var tdAllMail = $("tbody tr td:nth-of-type(4)");
+	var email = document.getElementById("email").value;
+
+	for (var i = 0; i < tdAllMail.length; i++) {
+		if (email === tdAllMail[i].innerHTML) {
+			return false;
+		}
 	}
 	return true;
 }
 
 function addtolist () {
 
-	if (!verifyForm ()) {
+	if (!verifyForm()) {
+		alert ("Merci de renseigner correctement tous les champs.")
+		return;
+	
+	}
+	if (!verifEmail()) {
+		alert ("Cette adresse email existe déjà.");
 		return;
 	}
 
@@ -37,21 +76,6 @@ function addtolist () {
 	column3.innerHTML += document.getElementById("lastname").value;
 	column4.innerHTML += document.getElementById("email").value;
 
+	column2.style.textTransform = "capitalize";
+	column3.style.textTransform = "uppercase";
 }
-/*
-
-
-{
-	if(document.studentform.lastname.value == "") {
-		alert ("Merci de renseigner correctement le nom");
-		return false;
-	}
-	else if(document.studentform.firstname.value == "") {
-		alert ("Merci de renseigner correctement le prénom");
-		return false;
-	}
-	if(document.studentform.email.value == "") {
-		alert ("Merci de renseigner correctement le prénom");
-		return false;
-	}
-}*/
