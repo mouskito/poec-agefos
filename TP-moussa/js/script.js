86bbce4192fd5f4265076ccb22f2ade69a800f90
amare <amarenighatu16@gmail.com>
2017-10-22 12:54:41 +0200

    Trying to add delete button, not yet

--- TP-moussa/js/script.js
+++ TP-moussa/js/script.js
@@ -1,13 +1,6 @@
-$("#signup").click(function (e) {
 
-    var x_nom = document.getElementById("nom").value;
-    var x_prenom = document.getElementById("prenom").value;
-    var x_email = document.getElementById("email").value;
-    $(".tbody").prepend("<tr><td></td></td><td>"+x_nom+"</td> <td>"+x_prenom+"</td> <td>"+x_email+"</td><td><input type=\"button\" class=\"bg-danger\"></td>\n" +
-        "                    <td><input type=\"button\" class=\"btn-success\"></td></tr>");
 
-    e.preventDefault()
-});
+// Form Validation
 
 function registration () {
 
@@ -17,7 +10,8 @@ function registration () {
 
     var email_regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,3})+$/;
 
-    if (nom === "") {
+
+    if (nom === "" ) {
         alert("Entrez votre nom!");
         document.form.nom.focus();
         document.form.nom.style.backgroundColor = "#f2786a";
@@ -35,71 +29,33 @@ function registration () {
         document.form.email.style.backgroundColor = "#f2786a";
         return false;
     }
-}
 
-
-// get the table by class
-/*var rIndex,
-    table = document.getElementsByClassName("table");*/
-
-// create a new row and cells
-// get value from input text
-// set the values into row cell's
-
-// Add row
-
-function addRow() {
 
 }
 
 
+// Add Rows
+
+$("#signup").click(function (e) {
+
+    var x_nom = document.getElementById("nom").value;
+    var x_prenom = document.getElementById("prenom").value;
+    var x_email = document.getElementById("email").value;
+
+    var remove = '<input type="button" id="delete" class="btn-danger" value="Delete">';
+    var edit ='<input type="button" id="edit" class="btn-success" value="Edit">';
+    $(".tbody").prepend("<tr><td>"+x_nom+"</td> <td>"+x_prenom+"</td> <td>"+x_email+"</td><td>"+remove+"</td><td>"+edit+"</td></tr>");
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-$(document).ready(function () {
-    $("#signup").click(function () {
-        var nom = $("#nom").val();
-        var prenom = $("#prenom").val();
-        var email = $("#email").val();
-        var add = "<tr><td><input type='checkbox' name='record'></td><td>" + nom + "</td><td>" + prenom + "</td><td>" + email +"</td></tr>";
-
-        $("table tbody").append(add);
-    });
-    // Find and remove selected table rows
-    $(".delete-row").click(function () {
-        $("table tbody").find('input[name="record"]').each(function () {
-            if ($(this).is(":checked")) {
-                $(this).parents("tr").remove();
-            }
-        })
-    });
+    e.preventDefault();
 
 });
-*/
+
+
+// Delete Rows
+
+$('table tbody').on('click', '#delete', function () {
+
+    $(this).closest('tr').remove();
+});
+
