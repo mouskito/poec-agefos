fdb828d41ed0458e8c9de3aef9cb055ff6eb55d9
Sebastien <grivel.seb@gmail.com>
2017-10-25 03:21:34 +0200

    On arrive dans le sprint final !

---
+++ Jeu_de_l'oie/js/script.js
@@ -0,0 +1,625 @@
+var tabCase = [
+    {
+        x: 0,
+        y: 0,
+        txt: " D",
+        color: 'rgb(60, 200, 60)',
+        type: 5
+    },
+    {
+        x: 50,
+        y: 0,
+        txt: "01",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 100,
+        y: 0,
+        txt: "02",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 150,
+        y: 0,
+        txt: "03",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 200,
+        y: 0,
+        txt: "04",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 250,
+        y: 0,
+        txt: "05",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 300,
+        y: 0,
+        txt: "06",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 350,
+        y: 0,
+        txt: "07",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 400,
+        y: 0,
+        txt: "08",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 450,
+        y: 0,
+        txt: "09",
+        color: 'rgb(200, 50, 200)',
+        type: 1
+    },
+    {
+        x: 500,
+        y: 0,
+        txt: "10",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 550,
+        y: 0,
+        txt: "11",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 600,
+        y: 0,
+        txt: "12",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 650,
+        y: 0,
+        txt: "13",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 700,
+        y: 0,
+        txt: "14",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 700,
+        y: 50,
+        txt: "15",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 700,
+        y: 100,
+        txt: "16",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 700,
+        y: 150,
+        txt: "17",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 700,
+        y: 200,
+        txt: "18",
+        color: 'rgb(200, 200, 50)',
+        type: 2
+    },
+    {
+        x: 700,
+        y: 250,
+        txt: "19",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 700,
+        y: 300,
+        txt: "20",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 700,
+        y: 350,
+        txt: "21",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 650,
+        y: 350,
+        txt: "22",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 600,
+        y: 350,
+        txt: "23",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 550,
+        y: 350,
+        txt: "24",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 500,
+        y: 350,
+        txt: "25",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 450,
+        y: 350,
+        txt: "26",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 400,
+        y: 350,
+        txt: "27",
+        color: 'rgb(20, 150, 150)',
+        type: 3
+    },
+    {
+        x: 350,
+        y: 350,
+        txt: "28",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 300,
+        y: 350,
+        txt: "29",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 250,
+        y: 350,
+        txt: "30",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 200,
+        y: 350,
+        txt: "31",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 150,
+        y: 350,
+        txt: "32",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 100,
+        y: 350,
+        txt: "33",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 50,
+        y: 350,
+        txt: "34",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 50,
+        y: 300,
+        txt: "35",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 50,
+        y: 250,
+        txt: "36",
+        color: 'rgb(200, 200, 50)',
+        type: 2
+    },
+    {
+        x: 50,
+        y: 200,
+        txt: "37",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 50,
+        y: 150,
+        txt: "38",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 50,
+        y: 100,
+        txt: "39",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 100,
+        y: 100,
+        txt: "40",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 150,
+        y: 100,
+        txt: "41",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 200,
+        y: 100,
+        txt: "42",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 250,
+        y: 100,
+        txt: "43",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 300,
+        y: 100,
+        txt: "44",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 350,
+        y: 100,
+        txt: "45",
+        color: 'rgb(200, 50, 200)',
+        type: 1
+    },
+    {
+        x: 400,
+        y: 100,
+        txt: "46",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 450,
+        y: 100,
+        txt: "47",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 500,
+        y: 100,
+        txt: "48",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 550,
+        y: 100,
+        txt: "49",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 600,
+        y: 100,
+        txt: "50",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 600,
+        y: 150,
+        txt: "51",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 600,
+        y: 200,
+        txt: "52",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 600,
+        y: 250,
+        txt: "53",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 550,
+        y: 250,
+        txt: "54",
+        color: 'rgb(20, 150, 150)',
+        type: 0
+    },
+    {
+        x: 500,
+        y: 250,
+        txt: "55",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 450,
+        y: 250,
+        txt: "56",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 400,
+        y: 250,
+        txt: "57",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 350,
+        y: 250,
+        txt: "58",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 300,
+        y: 250,
+        txt: "59",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 250,
+        y: 250,
+        txt: "60",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 200,
+        y: 250,
+        txt: "61",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 150,
+        y: 250,
+        txt: "62",
+        color: 'rgb(200, 200, 200)',
+        type: 0
+    },
+    {
+        x: 150,
+        y: 200,
+        txt: "  F",
+        color: 'rgb(200, 50, 50)',
+        type: 6
+    }
+];
+var timeout;
+var delay = 100;
+var nbPlayers;
+var players = [];
+var tokenColor = ["rouge", "bleu", "jaune", "vert"];
+var infoPlayer = $("#info")[0];
+var button = $("#lancer");
+var choiceOrder = $("#choice");
+var tabRollDiceOrder = [];
+var canvas = $("#canvasGame")[0];
+var ctx = canvas.getContext("2d");
+
+button.click(function () {
+    var firstRoll = parseInt(Math.random() * 6 + 1);
+    var secondRoll = parseInt(Math.random() * 6 + 1);
+    var playerCurrent;
+
+    alert(players[0].name + " a fait " + firstRoll + " et " + secondRoll + " pour un total de " + (firstRoll + secondRoll));
+    movePlayer(firstRoll + secondRoll);
+    refreshCanvas();
+    if (firstRoll === secondRoll) {
+        alert("Bravo " + players[0].name + ", vous venez d'effectuer un Double !\n Vous allez pouvoir relancer les dés !")
+    } else {
+        playerCurrent = players.shift();
+        players.push(playerCurrent);
+    }
+});
+
+choiceOrder.click(function () {
+    var playerCurrent;
+    var nbRollDice;
+
+    do {
+        nbRollDice = parseInt(Math.random() * 6 + 1);
+    } while (Math.max.apply(Math, tabRollDiceOrder) === nbRollDice);
+
+    tabRollDiceOrder.push(nbRollDice);
+    alert(players[0].name + " a fait un score de " + nbRollDice);
+    playerCurrent = players.shift();
+    players.push(playerCurrent);
+
+    if (tabRollDiceOrder.length === nbPlayers) {
+        setOrderPlayers();
+        button.css("display", "block");
+        choiceOrder.css("display", "none");
+    }
+});
+
+
+function movePlayer(deplacement) {
+    if (players[0].currentCase + deplacement > 63) {
+        alert("Oups " + players[0].name + ", vous êtes parti bien trop loin .. Vous revoilà au départ !");
+        players[0].currentCase = 0;
+        players[0].x = tabCase[0].x;
+        players[0].y = tabCase[0].y;
+    } else {
+        players[0].currentCase += deplacement;
+        players[0].x = tabCase[players[0].currentCase].x;
+        players[0].y = tabCase[players[0].currentCase].y;
+    }
+}
+
+
+function refreshCanvas() {
+    ctx.clearRect(0, 0, 750, 400);
+    
+    for (var i = 0; i < tabCase.length; i++) {
+        ctx.fillStyle = tabCase[i].color;
+        ctx.fillRect(tabCase[i].x, tabCase[i].y, 50, 50);
+        ctx.strokeRect(tabCase[i].x, tabCase[i].y, 50, 50);
+        ctx.font = "20px Impact";
+        ctx.fillStyle = "#000";
+        ctx.fillText(tabCase[i].txt, tabCase[i].x + 15, tabCase[i].y + 33);
+    }
+    for (var i = 0; i < nbPlayers; i++) {
+        ctx.beginPath();
+        ctx.fillStyle = players[i].codeColor;
+        ctx.arc((players[i].x + 25), (players[i].y + 25), 10, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+}
+
+function setOrderPlayers() {
+    var max = 0;
+    var id = 0;
+    var playerToMove;
+    var listPlayersOrdered = "";
+
+    for (var i = 0; i < tabRollDiceOrder.length; i++) {
+        if (max < tabRollDiceOrder[i]) {
+            max = tabRollDiceOrder[i];
+            id = i;
+        }
+    }
+    for (var i = 0; i < id; i++) {
+        playerToMove = players.shift();
+        players.push(playerToMove);
+    }
+    for (var i = 0; i < players.length; i++) {
+        players[i].positionInList = i;
+        players[i].x = 0;
+        players[i].y = 0;
+        players[i].currentCase = 0;
+        listPlayersOrdered += players[i].name + " ";
+    }
+    alert("L'ordre de jeu sera le suivant : " + listPlayersOrdered);
+}
+
+window.onload = function () {
+    function init() {
+
+        button.css("display", "none");
+        if (canvas.getContext) {
+            canvas.style.margin = "50px auto";
+            canvas.style.display = "block";
+            for (var i = 0; i < tabCase.length; i++) {
+                ctx.fillStyle = tabCase[i].color;
+                ctx.fillRect(tabCase[i].x, tabCase[i].y, 50, 50);
+                ctx.strokeRect(tabCase[i].x, tabCase[i].y, 50, 50);
+                ctx.font = "20px Impact";
+                ctx.fillStyle = "#000";
+                ctx.fillText(tabCase[i].txt, tabCase[i].x + 15, tabCase[i].y + 33);
+            }
+            createPlayers();
+        } else {
+            alert("La création du Canvas a échoué !");
+        }
+    }
+
+    function createPlayers() {
+        do {
+            nbPlayers = parseInt(prompt("Veuillez indiquer le nombre de joueurs : ", "(2, 3 ou 4)"));
+        } while (nbPlayers < 2 || nbPlayers > 4 || !nbPlayers || nbPlayers === "")
+        for (var i = 0; i < nbPlayers; i++) {
+            var player = {};
+
+            do {
+                player.name = prompt("Veuillez choisir un nom pour le Joueur n°" + (i + 1));
+                for (var c = 0; c < players.length; c++) {
+                    if (player.name === players[c].name) {
+                        alert("Un joueur possède déjà ce nom. \n Veuillez choisir un autre pseudonyme !");
+                        player.name = "";
+                        break;
+                    }
+                }
+            } while (!player.name || player.name === "");
+            do {
+                player.color = prompt("Bonjour " + player.name + "\nChoisis une couleur entre " + tokenColor[0] + " " + tokenColor[1] + " " + tokenColor[2] + " " + tokenColor[3]);
+                switch (player.color) {
+                    case tokenColor[0]:
+                        player.codeColor = 'rgb(150, 20, 20)';
+                        tokenColor[0] = "";
+                        break;
+                    case tokenColor[1]:
+                        player.codeColor = 'rgb(20, 20, 150)';
+                        tokenColor[1] = "";
+                        break;
+                    case tokenColor[2]:
+                        player.codeColor = 'rgb(150, 150, 20)';
+                        tokenColor[2] = "";
+                        break;
+                    case tokenColor[3]:
+                        player.codeColor = 'rgb(20, 150, 20)';
+                        tokenColor[3] = "";
+                        break;
+                    default:
+                        player.color = "";
+                        break;
+                }
+            } while (!player.color || player.color === "")
+            players[i] = player;
+        }
+    }
+    init();
+}
