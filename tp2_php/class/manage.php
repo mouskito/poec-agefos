fc4ab25bf20147e59bc7080cf451df58e841718e
Sebastien <grivel.seb@gmail.com>
2017-11-05 11:37:00 +0100

    Petit coup de Bootstrap

--- tp2_php/class/manage.php
+++ tp2_php/class/manage.php
@@ -85,34 +85,33 @@ function getClassById($classes, $id) {
             <tr>
               <td><?=++$i?></td>
               <td><?=$class->body ?></td>
-              <td>
-                <?php 
-                    $urlSubclassDel = '?route=subclass/delete';
-                    $urlSubclassEdit = '?route=subclass/edit';
-                    $urlSubclassAdd = '?route=subclass/add&id_class='.$class->id;
-                    $subclasses = $query2->fetchAll(PDO::FETCH_OBJ);
-                    if (sizeof($subclasses) == 0) {
-                      echo 'Pas de sous-catégorie disponible';
-                    } else {
-                        foreach($subclasses as $subclass) {
-                            echo $subclass->body;
-                            echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a
-                                    class="btn btn-info btn-xs"
-                                    href="'.$urlSubclassEdit.'&id_subclass='.$subclass->id.'">Modifier
-                                </a>';
-                            echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a
-                                    class="btn btn-warning btn-xs"
-                                    href="'.$urlSubclassDel.'&id_subclass='.$subclass->id.'">Supprimer
-                                </a>';
-                            echo '<br/>';
-                            }
-                    }
-                  echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a
-                         class="btn btn-success btn-xs"
-                        href="'.$urlSubclassAdd.'">Ajouter
-                        </a>';
-                 ?>
-              </td>
+                  <td>
+                    <?php 
+                        $urlSubclassDel = '?route=subclass/delete';
+                        $urlSubclassEdit = '?route=subclass/edit';
+                        $urlSubclassAdd = '?route=subclass/add&id_class='.$class->id;
+                        $subclasses = $query2->fetchAll(PDO::FETCH_OBJ);
+                        if (sizeof($subclasses) == 0) {
+                          echo 'Pas de sous-catégorie dispo  : ';
+                        } else {
+                            foreach($subclasses as $subclass) {
+                                echo '<div class="row" style="margin-bottom:7px"><div class="col-md-6">'.$subclass->body.'</div>';
+                                echo '<div class="col-md-3"><a
+                                        class="btn btn-info btn-xs"
+                                        href="'.$urlSubclassEdit.'&id_subclass='.$subclass->id.'">Modifier
+                                    </a></div>';
+                                echo '<div class="col-md-3"><a
+                                        class="btn btn-warning btn-xs"
+                                        href="'.$urlSubclassDel.'&id_subclass='.$subclass->id.'">Supprimer
+                                    </a></div></div>';
+                                }
+                        }
+                          echo '<div class="row"><div class="col-md-3 col-md-offset-9"><a
+                                 class="btn btn-success btn-xs"
+                                href="'.$urlSubclassAdd.'">Ajouter Sc
+                                </a></div></div>';
+                         ?>
+                    </td>
               <td>
                 <?php
                   $urlDel = '?route=class/delete&id_class=' . $class->id;
@@ -152,12 +151,14 @@ function getClassById($classes, $id) {
           <label for="body">Texte</label>
           <textarea name="body" required><?= $classEdit->body ?></textarea>
         </div>
-        <!--
+<!--
+        
         <p style="font-weight:bold">Sous-catégorie (optionnel) : </p>
         <div class="form-group" id="listSubclasses"></div>    
         <input type="button" name="addSubclass" value="Add one subclass" id="addSubclass">
         <input type="button" name="delSubclass" value="Delete one subclass" id="delSubclass">
-        -->
+        
+-->
         <input type="hidden" name="id_class" value="<?= $classEdit->id ?>">
         <input
           class="btn btn-primary"
